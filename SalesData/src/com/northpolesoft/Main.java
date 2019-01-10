9d2ad31bb7a2031a5b8118b2cbc056a2b16f81ac
ertugrul duran <duranertugrul@gmail.com>
2019-01-10 23:37:59 +0000

    project is rebased

--- SalesData/src/com/northpolesoft/Main.java
+++ SalesData/src/com/northpolesoft/Main.java
@@ -17,9 +17,7 @@ public class Main {
 		
 		System.out.println("Test 1");
 		System.out.println("Test 2");
-		System.out.println("Test 3");
 		System.out.println("Test 4");
-		System.out.println("Test 5");
 		System.out.println("Test 7");
 	}
 
