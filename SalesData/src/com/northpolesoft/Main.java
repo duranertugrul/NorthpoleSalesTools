1f5ee49d6b97d1900c6c08b4e978e849f698a602
ertugrul duran <duranertugrul@gmail.com>
2019-01-10 20:00:16 +0000

    Initial commit of project

---
+++ SalesData/src/com/northpolesoft/Main.java
@@ -0,0 +1,19 @@
+package com.northpolesoft;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		SalesData salesData = new SalesData();
+		
+		displayGreeting();
+		
+		salesData.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello Happy Sales People!!");
+		System.out.println("This App Shows Sales Data");
+	}
+
+
+}
