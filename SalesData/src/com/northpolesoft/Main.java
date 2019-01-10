792d42e888d46e478064e3a5437ec5e5de0927ae
ertugrul duran <duranertugrul@gmail.com>
2019-01-10 22:24:00 +0000

    Add simple output change

--- SalesData/src/com/northpolesoft/Main.java
+++ SalesData/src/com/northpolesoft/Main.java
@@ -13,7 +13,9 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("Hello Happy Sales People!!");
 		System.out.println("This App Shows Sales Data");
+		
 		System.out.println("Test 1");
+		System.out.println("Test 2");
 	}
 
 
