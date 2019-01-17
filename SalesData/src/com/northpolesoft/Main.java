545839014b2e2d8a5177eaff2aa1c375f96a70c0
ertugrul duran <duranertugrul@gmail.com>
2019-01-17 20:52:33 +0000

    hello 3 ert

--- SalesData/src/com/northpolesoft/Main.java
+++ SalesData/src/com/northpolesoft/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		
 		displayGreeting();
 		System.out.println("hello world");
-		System.out.println("hello world2");
+		System.out.println("hello 3");
 		salesData.display();
 	}
 	
